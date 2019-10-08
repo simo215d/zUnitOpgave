66ad27046613615edb99e8aca2db0db9c78bcfd3
Anders Petersen <AndP@easj.dk>
2019-10-08 11:50:57 +0200

    Initial commit

---
+++ src/TestSuite.java
@@ -0,0 +1,12 @@
+public abstract class TestSuite {
+
+    public void runTestSuite() {
+        // TODO
+    }
+
+    protected abstract void testList();
+
+    public void assertEquals(int expected, int actual) {
+        // TODO
+    }
+}
---
+++ test/TestClass.java
@@ -0,0 +1,15 @@
+public class TestClass extends TestSuite {
+    @Override
+    protected void testList() {
+        testThis();
+        testThat();
+    }
+
+    public void testThis() {
+        // TODO
+    }
+
+    public void testThat() {
+        // TODO
+    }
+}
